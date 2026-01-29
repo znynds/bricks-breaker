3e5083c5484f9ddf86edcdd4ebe67b0d57009b21
znynds <normal_one0711@163.com>
2026-01-29 13:56:40 +0800

    feat: create main scene with its node
    Initialize main scene structure and add its associated node components
    完成主场景的基础创建，同时添加其关联的节点组件，搭建主场景核心结构

---
+++ scripts/bottom.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "ball":
+		print("小球掉落")
+		#待补充，小球脚本未写
+	pass # Replace with function body.
